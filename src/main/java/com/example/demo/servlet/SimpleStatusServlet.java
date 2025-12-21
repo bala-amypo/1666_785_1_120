0ad025343410622525917302b08ee0414d29133d
bala-amypo <bala@amypo.in>
2025-12-21 13:23:15 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -3,8 +3,8 @@ package com.example.demo.servlet;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
-@WebServlet(urlPatterns = "/simple-status")
+@WebServlet(urlPatterns = "/simple-status") [cite: 479]
 public class SimpleStatusServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {}
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {} [cite: 480]
 }
