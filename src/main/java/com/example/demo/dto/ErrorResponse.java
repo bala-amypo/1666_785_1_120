8e0101af8213b13a55d068f767ae247b283c70b6
bala-amypo <bala@amypo.in>
2025-12-21 14:02:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ErrorResponse.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+import java.time.LocalDateTime;
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+    // Add Getters, Setters, and Constructor
+}
