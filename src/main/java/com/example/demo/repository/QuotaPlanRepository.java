8a46de80803f41cf02a447e089ae4bba39330032
bala-amypo <bala@amypo.in>
2025-12-21 14:20:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/QuotaPlanRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.QuotaPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuotaPlanRepository extends JpaRepository<QuotaPlan, Long> {
+    // No extra methods required beyond CRUD.
+}
