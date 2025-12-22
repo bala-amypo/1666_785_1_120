fa3d5e8cf3f8eb9ab41e82a73a5d617b8325a5eb
bala-amypo <bala@amypo.in>
2025-12-22 09:05:13 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -10,13 +10,11 @@ import java.io.IOException;
 public class SimpleStatusServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        // 1. Set the content type to text/plain
+ 
         resp.setContentType("text/plain"); 
-        
-        // 2. Set HTTP status to 200 OK
+
         resp.setStatus(HttpServletResponse.SC_OK); 
-        
-        // 3. Write the required message to the response body
+ 
         resp.getWriter().write("API Rate Limiter & Quota Manager is running");
     }
 }
