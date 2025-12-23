c5a08bc0d8f97b4d3d0dd7833a8f94a5d5eea001
bala-amypo <bala@amypo.in>
2025-12-23 09:18:57 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,7 +1,7 @@
-// package com.example.demo.security;
+package com.example.demo.security;
 
 
-// public class CustomUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
-//     @Override
-//     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String email) { return null; }
-// }
+public class CustomUserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Override
+    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String email) { return null; }
+}
