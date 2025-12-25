295267bf4cf299244a387f71a05797b19781d56e
bala-amypo <bala@amypo.in>
2025-12-25 16:15:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/KeyExemptionController.java
+++ src/main/java/com/example/demo/controller/KeyExemptionController.java
@@ -1,33 +1,12 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.KeyExemption;
 import com.example.demo.service.KeyExemptionService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
-@RestController
-@RequestMapping("/api/key-exemptions")
-@Tag(name = "Key Exemptions")
 public class KeyExemptionController {
-    private final KeyExemptionService exemptionService;
 
-    public KeyExemptionController(KeyExemptionService exemptionService) {
-        this.exemptionService = exemptionService;
-    }
+    private KeyExemptionService exemptionService;
 
-    @PostMapping
-    public KeyExemption createExemption(@RequestBody KeyExemption exemption) {
-        return exemptionService.createExemption(exemption);
-    }
-
-    @GetMapping("/key/{keyId}")
-    public KeyExemption getByApiKey(@PathVariable Long keyId) {
-        return exemptionService.getExemptionByKey(keyId).orElse(null);
-    }
-
-    @GetMapping
-    public List<KeyExemption> getAll() {
+    public Iterable<?> getAll() {
         return exemptionService.getAllExemptions();
     }
 }
