fd99a7f095da17c1d3a1c62173151de9a25be788
bala-amypo <bala@amypo.in>
2025-12-26 15:38:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/QuotaPlan.java
+++ src/main/java/com/example/demo/entity/QuotaPlan.java
@@ -1,20 +1,14 @@
 package com.example.demo.entity;
-
+@Entity
 public class QuotaPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String planName;
-    private int dailyLimit;
+
     private boolean active;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getPlanName() { return planName; }
-    public void setPlanName(String planName) { this.planName = planName; }
-
-    public int getDailyLimit() { return dailyLimit; }
-    public void setDailyLimit(int dailyLimit) { this.dailyLimit = dailyLimit; }
-
-    public boolean isActive() { return active; }
-    public void setActive(boolean active) { this.active = active; }
+    public boolean isActive() {
+        return active;
+    }
 }
