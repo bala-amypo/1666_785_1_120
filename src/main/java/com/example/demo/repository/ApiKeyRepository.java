ca3607f28d89cc93233b2063bc5474e180cc7d96
bala-amypo <bala@amypo.in>
2025-12-26 15:50:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApiKeyRepository.java
+++ src/main/java/com/example/demo/repository/ApiKeyRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.repository;
 
 import java.util.*;
-import com.example.demo.entity.ApiKey;
+import java.util.List;
+
 
 public interface ApiKeyRepository {
-    Optional<ApiKey> findById(Long id);
-    Optional<ApiKey> findByKeyValue(String value);
+    findById(Long id);
+    findByKeyValue(String value);
     List<ApiKey> findAll();
     ApiKey save(ApiKey key);
 }
